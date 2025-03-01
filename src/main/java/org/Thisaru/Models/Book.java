c38ad713c829c6e7e46331cb04a2a2a9f321f41e
Induwara Thisaru <168101533+induwara-inivos@users.noreply.github.com>
2025-03-01 10:03:38 +0530

    initializing project

---
+++ src/main/java/org/Thisaru/Models/Book.java
@@ -0,0 +1,77 @@
+package org.Thisaru.Models;
+
+public class Book {
+    private int id;
+    private String title;
+    private int authorId;
+    private String isbn;
+    private int publicationYear;
+    private int price;
+    private int stockQuantity;
+
+    public Book(int id, String title, int authorId, String isbn, int publicationYear, int price, int stockQuantity) {
+        this.id = id;
+        this.title = title;
+        this.authorId = authorId;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
---
+++ src/main/java/org/Thisaru/Models/Order.java
@@ -0,0 +1,4 @@
+package org.Thisaru.Models;
+
+public class Order {
+}
