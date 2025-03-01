ce0c9aefeb533c1566cd8303393e5848eb7adcf1
Induwara Thisaru <168101533+induwara-inivos@users.noreply.github.com>
2025-03-01 10:12:36 +0530

    finalized models

--- src/main/java/org/Thisaru/Models/Cart.java
+++ src/main/java/org/Thisaru/Models/Cart.java
@@ -1,4 +1,21 @@
 package org.Thisaru.Models;
 
+import java.util.Map;
+
 public class Cart {
+    private int customerId;
+    private Map<Integer, Integer> items;
+
+    public Cart(int customerId, Map<Integer, Integer> items) {
+        this.customerId = customerId;
+        this.items = items;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public Map<Integer, Integer> getItems() {
+        return items;
+    }
 }
--- src/main/java/org/Thisaru/Models/Order.java
+++ src/main/java/org/Thisaru/Models/Order.java
@@ -1,4 +1,49 @@
 package org.Thisaru.Models;
 
+import java.util.Map;
+
 public class Order {
+    private int orderId;
+    private int customerId;
+    private Map<Integer, Integer> orderedItems;
+    private double totalPrice;
+
+    public Order(int orderId, int customerId, Map<Integer, Integer> orderedItems, double totalPrice) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.orderedItems = orderedItems;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public Map<Integer, Integer> getOrderedItems() {
+        return orderedItems;
+    }
+
+    public void setOrderedItems(Map<Integer, Integer> orderedItems) {
+        this.orderedItems = orderedItems;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
